fb61a0241a7732971a3db04ada7e90800f3b8402
JDlnwza007 <68010147@kmitl.ac.th>
2026-01-21 15:18:41 +0700

    03fn

--- src/practicals/p03/fizzbuzz.ts
+++ src/practicals/p03/fizzbuzz.ts
@@ -1,1 +1,19 @@
-// Write your code below
+const score =Number(process.argv[2]);
+    for (let i = 1; i <= score; i++) {
+        
+        if (i % 15 === 0) {
+            console.log('FizzBuzz');
+        } 
+        
+        else if (i % 3 === 0) {
+            console.log('Fizz');
+        } 
+        
+        else if (i % 5 === 0) {
+            console.log('Buzz');
+        } 
+
+        else {
+            console.log(i.toString());
+        }
+    }
