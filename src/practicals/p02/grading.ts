423026e91f4999411b61539238e7b8e36008c983
JDlnwza007 <68010147@kmitl.ac.th>
2026-01-21 15:37:16 +0700

    02

--- src/practicals/p02/grading.ts
+++ src/practicals/p02/grading.ts
@@ -1,16 +1,16 @@
-const score =Number(process.argv[2]);
+const x = Number(process.argv[2]);
 
-if (isNaN(score) || score <0 || score >100) {
-console.log("Invalid input");
-}else if (score >=80) {
-console.log("A");
-}else if (score >=70) {
-console.log("B");
-}else if (score >=60) {
-console.log("C");
-}else if (score >=50) {
-console.log("D");
-}else {
-console.log("F");
+if (isNaN(x) || x < 0 || x > 100) {
+    console.log("Invalid input");
+} else if (x >= 80) {
+    console.log("A");
+} else if (x >= 70) {
+    console.log("B");
+} else if (x >= 60) {
+    console.log("C");
+} else if (x >= 50) {
+    console.log("D");
+} else {
+    console.log("F");
 }
 
